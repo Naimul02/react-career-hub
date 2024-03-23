f0c0c09a18fe4d25df58d78773418c3d66bcdbf2
Naimul02 <naimulislum39@gmail.com>
2024-03-23 13:02:16 +0600

    error page updated

--- src/component/ErrorPage/ErrorPage.jsx
+++ src/component/ErrorPage/ErrorPage.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
     <div>
-      <h2>Opps!!</h2>
-      <Link to="/">
-        <button className="btn">Go back to home</button>
+      <h2 className="text-2xl font-semibold">Opps!!!</h2>
+      <Link to="/" className="text-blue-600 underline">
+        Back to home
       </Link>
     </div>
   );
