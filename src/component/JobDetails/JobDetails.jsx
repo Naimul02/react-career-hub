7a2c0e7eefa0a3aedc6dc65027de6be2e7a57c0a
Naimul02 <naimulislum39@gmail.com>
2024-03-24 20:57:33 +0600

    finished theproject

--- src/component/JobDetails/JobDetails.jsx
+++ src/component/JobDetails/JobDetails.jsx
@@ -10,7 +10,7 @@ const JobDetails = () => {
   console.log(job);
 
   const handleApplyJob = () => {
-    saveJobApplication(id);
+    saveJobApplication(idInt);
     toast("You have applied Succesfully");
   };
   return (
