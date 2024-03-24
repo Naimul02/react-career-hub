fd54b6f7b08b46211e430fa476d095a3c466c2e0
Naimul02 <naimulislum39@gmail.com>
2024-03-24 11:46:44 +0600

    commit no 6

--- src/component/JobDetails/JobDetails.jsx
+++ src/component/JobDetails/JobDetails.jsx
@@ -1,14 +1,20 @@
 import { useLoaderData, useParams } from "react-router-dom";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { saveJobApplication } from "../../utility/localStorage";
 const JobDetails = () => {
   const jobs = useLoaderData();
   const { id } = useParams();
   const idInt = parseInt(id);
   const job = jobs.find((job) => job.id === idInt);
   console.log(job);
+
+  const handleApplyJob = () => {
+    saveJobApplication(id);
+    toast("You have applied Succesfully");
+  };
   return (
     <div>
-      
       <div className="grid md:grid-cols-4 gap-4">
         <div className="border md:col-span-3">
           <h2 className="text-4xl">Details Coming here</h2>
@@ -17,9 +23,12 @@ const JobDetails = () => {
         </div>
         <div className="border md:col-span-1">
           <h2 className="text-2xl">Side Things</h2>
-          <button className="btn btn-primary w-full">Apply Now</button>
+          <button onClick={handleApplyJob} className="btn btn-primary w-full">
+            Apply Now
+          </button>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
